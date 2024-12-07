f4e36f86bcea3ae65cf1be2a4b99a54607561aa6
funnyphatguy <contius32@gmail.com>
2024-12-07 13:56:53 +0300

    KTB-04 добавил логику парсинга строк и дата класс, создал экземпляры дата класса и добавил их в список словаря.

--- src/main/kotlin/FileTxt.kt
+++ src/main/kotlin/FileTxt.kt
@@ -2,13 +2,31 @@ package org.example
 
 import java.io.File
 
+data class Word(
+    val original: String,
+    val translation: String,
+    val correctAnswersCount: Int = 0
+)
 
 fun main() {
-    val wordsFile: File = File("words.txt")
+    val wordsFile = File("words.txt")
 
+    val dictionary: MutableList<Word> = mutableListOf()
 
-
-    val listOfWorlds = wordsFile.forEachLine { println(it) }
-
+    val list = wordsFile.forEachLine { line ->
+        val cells = line.split("|")
+        val word = Word(
+            original = cells[0],
+            translation = cells[1],
+            correctAnswersCount = cells.getOrNull(2)?.toIntOrNull() ?: 0
+        )
+        dictionary.add(word).also { println(dictionary) }
     }
+}
+
+
+
+
+
+
 
