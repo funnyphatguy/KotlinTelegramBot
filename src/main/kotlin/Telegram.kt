e3340bccbf7492dff60e91252b4297534e14e979
funnyphatguy <contius32@gmail.com>
2024-12-29 23:30:12 +0300

    KTB-17 теперь по выбору в боте пункта "статистика" показывается статистика

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -12,6 +12,8 @@ fun main(args: Array<String>) {
 
     val chatIdRegex: Regex = """"chat":\s*\{\s*"id":\s*(\d+)\s*(?=[,}])""".toRegex()
 
+    val dataRegex: Regex = "\"data\":\"(.+?)\"".toRegex()
+
     while (true) {
         Thread.sleep(2000)
         val updates: String = botService.getUpdates(updateId)
@@ -26,16 +28,29 @@ fun main(args: Array<String>) {
         val chatId = chatIdRegex.find(updates)?.groups?.get(1)?.value
         println("Chat ID: $chatId")
 
+        val data = dataRegex.find(updates)?.groups?.get(1)?.value
+        println("Дата ${data}")
+
         val trainer: LearnWordsTrainer = LearnWordsTrainer()
 
-        if (text != null) {
-            if (text.lowercase() == "hello") {
-               botService.sendMessage(chatId, messageText = "Hello")
-            } else if (text.lowercase() == "menu") {
-                if (chatId != null) {
-                    botService.sendMenu(chatId)
-                }
-            }
+
+        val statistics = trainer.getStatistics()
+        println(statistics)
+
+
+        if (data?.lowercase() == STATISTICS_RESPONSE && chatId != null) {
+            botService.sendMessage(chatId, messageText = "Выучено ${statistics.learnedCount} из ${statistics.totalCount} слов | ${statistics.percent}%")
+        }
+
+        if (data?.lowercase() == STATISTICS_RESPONSE && chatId != null) {
+            botService.sendMessage(chatId, messageText = "Выучено ${statistics.learnedCount} из ${statistics.totalCount} слов | ${statistics.percent}%")
+        }
+
+        if (text?.lowercase() == "hello" && chatId != null) {
+            botService.sendMessage(chatId, messageText = "Hello")
+        }
+        if (text?.lowercase() == "menu" && chatId != null) {
+            botService.sendMenu(chatId)
         }
     }
 }
